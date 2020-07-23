6f2980a84a224110db160ade0b5a8360d0aa72cc
shenjie <1059459348@qq.com>
2020-07-23 22:49:54 +0800

    代码更新

--- spu/src/main/java/com/sj/spu/SpuApplication.java
+++ spu/src/main/java/com/sj/spu/SpuApplication.java
@@ -2,8 +2,12 @@ package com.sj.spu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient   //声明是一个客户端  注册到注册中心
+@EnableFeignClients
 public class SpuApplication {
 
     public static void main(String[] args) {
--- user/src/main/java/com/sj/user/UserApplication.java
+++ user/src/main/java/com/sj/user/UserApplication.java
@@ -3,9 +3,11 @@ package com.sj.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient   //声明是一个客户端  注册到注册中心
+@EnableFeignClients
 public class UserApplication {
 
     public static void main(String[] args) {
